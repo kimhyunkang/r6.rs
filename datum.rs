aec9b6d1580ec7750e970c2bfac6452403a1523b
kimhyunkang <kimhyunkang@gmail.com>
2013-05-08 15:17:13 +0900

    separate datum from parser

---
+++ datum.rs
@@ -0,0 +1,116 @@
+use rational::Rational;
+use complex::Complex;
+
+#[deriving(Eq)]
+pub enum LDatum {
+    LIdent(~str),
+    LString(~str),
+    LChar(char),
+    LBool(bool),
+    LECmplx(Complex<Rational>),
+    LICmplx(Complex<f64>),
+    LRational(Rational),
+    LFloat(f64),
+    LInt(int),
+    LCons(@LDatum, @LDatum),
+    LNil,
+}
+
+impl ToStr for LDatum {
+    fn to_str(&self) -> ~str {
+        do io::with_str_writer |wr| {
+            LDatum::write_ldatum(wr, self)
+        }
+    }
+
+}
+
+pub impl LDatum {
+    fn to_list(&self) -> Option<~[@LDatum]> {
+        match *self {
+            LCons(h, t) => datum_to_list(h, t),
+            LNil => Some(~[]),
+            _ => None,
+        }
+    }
+}
+
+priv fn datum_to_list(head: @LDatum, tail: @LDatum) -> Option<~[@LDatum]> {
+    let mut x: @LDatum = tail;
+    let mut list_flag = false;
+
+    let list =
+    do vec::build |push| {
+        push(head);
+        loop {
+            match *x {
+                LCons(h, t) => {
+                    push(h);
+                    x = t;
+                },
+                LNil => break,
+                _ => {
+                    list_flag = true;
+                    break
+                },
+            }
+        }
+    };
+
+    if list_flag {
+        None
+    } else {
+        Some(list)
+    }
+}
+
+priv impl LDatum {
+    fn write_ldatum(wr: @io::Writer, &v: &LDatum) {
+        match v {
+            LIdent(s) => wr.write_str(s),
+            LString(s) => wr.write_str(fmt!("%?", copy s)),
+            LChar(' ') => wr.write_str("#\\space"),
+            LChar('\n') => wr.write_str("#\\newline"),
+            LChar(c) => {
+                wr.write_str("#\\");
+                wr.write_char(c);
+            }
+            LBool(true) => wr.write_str("#t"),
+            LBool(false) => wr.write_str("#f"),
+            LRational(f) => {
+                if f.is_negative() {
+                    wr.write_char('-');
+                }
+                wr.write_str(f.denominator().to_str());
+                wr.write_char('/');
+                wr.write_str(f.numerator().to_str());
+            },
+            LFloat(f) => wr.write_str(f.to_str()),
+            LInt(z) => wr.write_str(z.to_str()),
+            LICmplx(c) => wr.write_str(c.to_str()),
+            LECmplx(c) => wr.write_str(c.to_str()),
+            LCons(head, tail) => {
+                wr.write_char('(');
+                LDatum::write_ldatum(wr, head);
+                LDatum::write_list(wr, tail);
+            },
+            LNil => wr.write_str("()"),
+        }
+    }
+
+    fn write_list(wr: @io::Writer, &v: &LDatum) {
+        match v {
+            LCons(head, tail) => {
+                wr.write_str(", ");
+                LDatum::write_ldatum(wr, head);
+                LDatum::write_list(wr, tail);
+            },
+            LNil => wr.write_char(')'),
+            _ => {
+                wr.write_str(" . ");
+                LDatum::write_ldatum(wr, &v);
+                wr.write_char(')');
+            },
+        }
+    }
+}
