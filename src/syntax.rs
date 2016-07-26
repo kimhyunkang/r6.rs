446dc8a930429bcc269e720ecd7edadc85129f80
Hugo Kim <hukim@riotgames.com>
2016-07-25 18:28:48 -0700

    Matchers work

--- src/syntax.rs
+++ src/syntax.rs
@@ -1,19 +1,23 @@
 use std::borrow::Cow;
 use std::collections::HashSet;
+use std::fmt::Debug;
 
 use datum::{Datum, SimpleDatum};
 use error::{MacroError, MacroErrorKind};
 
+#[derive(Debug)]
 struct CompiledPattern {
     vars: HashSet<Cow<'static, str>>,
     pattern: Pattern
 }
 
+#[derive(Debug)]
 enum Pattern {
     List(Vec<SubPattern>),
-    DelimitedList(Vec<SubPattern>, Box<CompiledPattern>, Vec<SubPattern>)
+    DelimitedList(Vec<SubPattern>, Box<SubPattern>, Vec<SubPattern>)
 }
 
+#[derive(Debug)]
 enum SubPattern {
     Underscore,
     Var(Cow<'static, str>),
@@ -21,25 +25,22 @@ enum SubPattern {
     Pattern(CompiledPattern)
 }
 
-#[derive(Debug, PartialEq)]
-struct PatternMatch<T> {
-    vars: HashSet<Cow<'static, str>>,
-    matches: Vec<MatchData<T>>
-}
+type PatternMatch<T> = Vec<MatchData<T>>;
 
 #[derive(Debug, PartialEq)]
 enum MatchData<T> {
     Var(Cow<'static, str>, Datum<T>),
-    Repeats(HashSet<Cow<'static, str>>, Vec<PatternMatch<T>>)
+    Repeated(HashSet<Cow<'static, str>>, Vec<PatternMatch<T>>)
 }
 
 impl CompiledPattern {
     fn compile<T>(literals: &HashSet<Cow<'static, str>>, datum: &Datum<T>)
             -> Result<CompiledPattern, MacroError>
-        where T: Clone + PartialEq
+        where T: Clone + PartialEq + Debug
     {
         let (form, tail) = datum.improper_list();
         if tail.is_some() {
+            println!("form: {:?}, tail: {:?}", form, tail);
             return Err(MacroError {
                 kind: MacroErrorKind::NotImplemented,
                 desc: "Dotted list pattern is not implemented yet".to_string()
@@ -53,7 +54,7 @@ impl CompiledPattern {
                     vars: HashSet::new(),
                     pattern: Pattern::List(Vec::new())
                 }),
-            &[chunk] => {
+            &[_] => {
                 Pattern::compile_subpatterns(literals, &form)
                         .map(|(vars, pats)| CompiledPattern {
                             vars: vars,
@@ -69,8 +70,8 @@ impl CompiledPattern {
                     Some((rep, rest)) => {
                         let (vars0, pats0) = try!(Pattern::compile_subpatterns(literals, rest));
                         let mut vars = vars0;
-                        let repeating = try!(CompiledPattern::compile(literals, rep));
-                        try!(union_vars(&mut vars, repeating.vars.clone()));
+                        let (vars1, repeating) = try!(SubPattern::compile(literals, rep));
+                        try!(union_vars(&mut vars, vars1));
                         let (vars2, pats2) = try!(Pattern::compile_subpatterns(literals, suffix));
                         try!(union_vars(&mut vars, vars2));
                         Ok(CompiledPattern {
@@ -102,10 +103,7 @@ impl CompiledPattern {
                         let mut m = try!(sp.compute_match(&elem));
                         matches.append(&mut m);
                     }
-                    Ok(PatternMatch {
-                        vars: self.vars.clone(),
-                        matches: matches
-                    })
+                    Ok(matches)
                 },
                 Pattern::DelimitedList(ref prefix, ref repeat, ref suffix) => {
                     if list.len() < prefix.len() + suffix.len() {
@@ -121,25 +119,19 @@ impl CompiledPattern {
                     let rep_start = prefix.len();
                     let rep_end = list.len() - suffix.len();
 
-                    let repeats = Vec::new();
+                    let mut repeats = Vec::new();
                     for elem in list[rep_start .. rep_end].iter() {
                         let m = try!(repeat.compute_match(elem));
                         repeats.push(m);
                     }
-                    matches.push(MatchData::Repeats(PatternMatch {
-                        vars: repeat.vars.clone(),
-                        matches: repeats
-                    }));
+                    matches.push(MatchData::Repeated(repeat.vars(), repeats));
 
                     for (sp, elem) in suffix.iter().zip(list[rep_end ..].iter()) {
                         let mut m = try!(sp.compute_match(&elem));
                         matches.append(&mut m);
                     }
 
-                    Ok(PatternMatch {
-                        vars: self.vars.clone(),
-                        matches: matches
-                    })
+                    Ok(matches)
                 }
             }
         } else {
@@ -166,7 +158,7 @@ fn union_vars(lhs: &mut HashSet<Cow<'static, str>>, rhs: HashSet<Cow<'static, st
 impl Pattern {
     fn compile_subpatterns<T>(literals: &HashSet<Cow<'static, str>>, data: &[Datum<T>])
             -> Result<(HashSet<Cow<'static, str>>, Vec<SubPattern>), MacroError>
-        where T: Clone + PartialEq
+        where T: Clone + PartialEq + Debug
     {
         let subpats: Result<Vec<(HashSet<Cow<'static, str>>, SubPattern)>, MacroError> =
                 data.iter().map(|sp| SubPattern::compile(literals, sp)).collect();
@@ -184,7 +176,7 @@ impl Pattern {
 impl SubPattern {
     fn compile<T>(literals: &HashSet<Cow<'static, str>>, datum: &Datum<T>)
             -> Result<(HashSet<Cow<'static, str>>, SubPattern), MacroError>
-        where T: Clone + PartialEq
+        where T: Clone + PartialEq + Debug
     {
         match datum {
             &Datum::Cons(_) | &Datum::Vector(_) =>
@@ -196,7 +188,9 @@ impl SubPattern {
                 } else if literals.contains(sym) {
                     Ok((HashSet::new(), SubPattern::Const(SimpleDatum::Sym(sym.clone()))))
                 } else {
-                    Ok((HashSet::new(), SubPattern::Var(sym.clone())))
+                    let mut set = HashSet::new();
+                    set.insert(sym.clone());
+                    Ok((set, SubPattern::Var(sym.clone())))
                 },
             _ =>
                 if let Some(c) = SimpleDatum::from_datum(datum.clone()) {
@@ -207,18 +201,6 @@ impl SubPattern {
                         desc: format!("Invalid datum in macro pattern")
                     })
                 }
-        }
-    }
-
-    fn vars(&self) -> Option<HashSet<Cow<'static, str>>> {
-        match self {
-            &SubPattern::Var(ref v) => {
-                let mut set = HashSet::new();
-                set.insert(v.clone());
-                Some(set)
-            },
-            &SubPattern::Pattern(ref pat) => Some(pat.vars.clone()),
-            _ => None
         }
     }
 
@@ -233,32 +215,47 @@ impl SubPattern {
                 } else {
                     Err(())
                 },
-            &SubPattern::Pattern(ref pat) => pat.compute_match(datum).map(|v| v.matches)
+            &SubPattern::Pattern(ref pat) => pat.compute_match(datum)
+        }
+    }
+
+    fn vars(&self) -> HashSet<Cow<'static, str>> {
+        match self {
+            &SubPattern::Var(ref sym) => {
+                let mut set = HashSet::new();
+                set.insert(sym.clone());
+                set
+            },
+            &SubPattern::Pattern(ref pat) => pat.vars.clone(),
+            _ => HashSet::new()
         }
     }
 }
 
 #[cfg(test)]
-mod test {
+mod test_matches {
     use std::borrow::Cow;
     use std::collections::HashSet;
-    use std::iter::FromIterator;
 
-    use super::{CompiledPattern, PatternMatch, MatchData};
+    use super::{CompiledPattern, MatchData};
     use datum::Datum;
     use number::Number;
     use parser::Parser;
 
     macro_rules! assert_matches {
-        ($pattern:expr, $datum:expr => $result:expr) => (
+        ([$($lits:expr),*] $pattern:expr, $datum:expr => $result:expr) => (
+            let literals = vec![$($lits),*].into_iter().map(Cow::Borrowed).collect();
+
             let mut pat_parser = Parser::new($pattern.as_bytes());
             let pattern = pat_parser.parse_full::<()>().expect("Failed to parse pattern");
 
             let mut datum_parser = Parser::new($datum.as_bytes());
             let datum = datum_parser.parse_full::<()>().expect("Failed to parse datum");
 
-            let compiled_pattern = CompiledPattern::compile(&HashSet::new(), &pattern)
+            let compiled_pattern = CompiledPattern::compile(&literals, &pattern)
                     .expect("Failed to compile pattern");
+
+            println!("compiled_pattern: {:?}", compiled_pattern);
 
             assert_eq!(Ok($result), compiled_pattern.compute_match(&datum));
         )
@@ -266,11 +263,81 @@ mod test {
 
     #[test]
     fn test_simple_pattern() {
-        let expected = PatternMatch {
-            vars: HashSet::from_iter(vec![Cow::Borrowed("a"), Cow::Borrowed("b")]),
-            matches: vec![MatchData::Var(Cow::Borrowed("a"), num!(1)), MatchData::Var(Cow::Borrowed("b"), num!(2))]
-        };
+        let expected = vec![MatchData::Var(Cow::Borrowed("a"), num!(1)), MatchData::Var(Cow::Borrowed("b"), num!(2))];
 
-        assert_matches!("(a b)", "(1 2)" => expected);
+        assert_matches!([] "(a b)", "(1 2)" => expected);
+    }
+
+    #[test]
+    fn test_nested_pattern() {
+        let expected = vec![
+            MatchData::Var(Cow::Borrowed("a"), num!(1)),
+            MatchData::Var(Cow::Borrowed("b"), num!(2)),
+            MatchData::Var(Cow::Borrowed("c"), list!(num!(3))),
+        ];
+
+        assert_matches!([] "(a (b) c)", "(1 (2) (3))" => expected);
+    }
+
+    #[test]
+    fn test_match_literals() {
+        let expected = vec![
+            MatchData::Var(Cow::Borrowed("a"), num!(1)),
+            MatchData::Var(Cow::Borrowed("c"), list!(num!(3))),
+        ];
+
+        assert_matches!(["b"] "(a (b) c)", "(1 (b) (3))" => expected);
+    }
+
+    #[test]
+    fn test_match_underscore() {
+        let expected = vec![
+            MatchData::Var(Cow::Borrowed("a"), num!(1)),
+            MatchData::Var(Cow::Borrowed("c"), list!(num!(3))),
+        ];
+
+        assert_matches!([] "(a _ c)", "(1 (2) (3))" => expected);
+    }
+
+    #[test]
+    fn test_repeated_variable() {
+        let mut vars = HashSet::new();
+        vars.insert(Cow::Borrowed("b"));
+
+        let expected = vec![
+            MatchData::Var(Cow::Borrowed("a"), num!(1)),
+            MatchData::Repeated(vars, vec![
+                                vec![MatchData::Var(Cow::Borrowed("b"), num!(2))],
+                                vec![MatchData::Var(Cow::Borrowed("b"), num!(3))],
+                                vec![MatchData::Var(Cow::Borrowed("b"), num!(4))]
+                                ]),
+            MatchData::Var(Cow::Borrowed("c"), num!(5)),
+        ];
+
+        assert_matches!([] "(a b ... c)", "(1 2 3 4 5)" => expected);
+    }
+
+    #[test]
+    fn test_repeated_pattern() {
+        let mut vars = HashSet::new();
+        vars.insert(Cow::Borrowed("b"));
+        vars.insert(Cow::Borrowed("c"));
+
+        let expected = vec![
+            MatchData::Var(Cow::Borrowed("a"), num!(1)),
+            MatchData::Repeated(vars, vec![
+                                    vec![
+                                        MatchData::Var(Cow::Borrowed("b"), num!(2)),
+                                        MatchData::Var(Cow::Borrowed("c"), num!(3))
+                                    ],
+                                    vec![
+                                        MatchData::Var(Cow::Borrowed("b"), num!(4)),
+                                        MatchData::Var(Cow::Borrowed("c"), num!(5))
+                                    ]
+                                ]),
+            MatchData::Var(Cow::Borrowed("d"), list!(num!(6), num!(7))),
+        ];
+
+        assert_matches!([] "(a (b c) ... d)", "(1 (2 3) (4 5) (6 7))" => expected);
     }
 }
